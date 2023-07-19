52d9476ac7d0e3d08a6735e980f8ee7d340b5fb5
yhyancy <2397856503@qq.com>
2023-07-19 19:17:55 +0800

    feat: update

--- packages/y-stylelint-config/__tests__/rules.validate.test.js
+++ packages/y-stylelint-config/__tests__/rules.validate.test.js
@@ -1,28 +1,29 @@
 const assert = require('assert');
 const stylelint = require('stylelint');
 const path = require('path');
-// describe('test/rules-validate.test.js', () => {
-//     it('Validate default', async () => {
-//       const filePaths = [path.join(__dirname, './fixtures/index.css')];
+const { describe,it } = require('jest-circus');
+describe('test/rules-validate.test.js', () => {
+    it('Validate default', async () => {
+      const filePaths = [path.join(__dirname, './fixtures/index.css')];
   
-//       const result = await stylelint.lint({
-//         configFile: path.join(__dirname, '../index.js'),
-//         files: filePaths,
-//         fix: false,
-//       });
+      const result = await stylelint.lint({
+        configFile: path.join(__dirname, '../index.js'),
+        files: filePaths,
+        fix: false,
+      });
 
 
-//       console.log('result',result)
-//       if (result && result.errored) {
-//         const filesResult = JSON.parse(result.output || '[]') || [];
-//         filesResult.forEach((fileResult) => {
-//           console.log(`========= ${filePaths} ==========`);
-//           console.log(fileResult.warnings);
-//         });
+      console.log('result',result)
+      if (result && result.errored) {
+        const filesResult = JSON.parse(result.output || '[]') || [];
+        filesResult.forEach((fileResult) => {
+          console.log(`========= ${filePaths} ==========`);
+          console.log(fileResult.warnings);
+        });
   
-//         assert.ok(filesResult.length !== 0);
-//       }
-//     });
+        assert.ok(filesResult.length !== 0);
+      }
+    });
 
-//   });
+  });
   
